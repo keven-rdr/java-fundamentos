a40eb0d0993ab8e8c55fbce5b1c50ebd50ca6921
KevenRodrigues6868 <kevenlucas2015@gmail.com>
2025-05-28 16:22:58 -0300

    refatorando o banknotes para maneira mais abreviada e adicionando o novo exercio de TimeConversion

--- src/Banknotes.java
+++ src/Banknotes.java
@@ -4,33 +4,19 @@ import java.io.InputStreamReader;
 
 public class Banknotes {
     public static void main(String[] args) throws IOException {
-        InputStreamReader isr = new InputStreamReader(System.in);
-        BufferedReader br = new BufferedReader(isr);
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
+       int N = Integer.parseInt(br.readLine());
 
-            System.out.println(N);
-            int A = (N / 100) * 100;
+       System.out.println(N);
 
-            int RESA = A / 100;
-            System.out.println(RESA+" nota(s) de R$ 100,00");
+       int[] notes = {100, 50, 20, 10, 5, 2, 1};
 
-            int RESB = (N % 100) / 50;
-            System.out.println(RESB+" nota(s) de R$ 50,00");
-
-            int RESC = ((N % 100) % 50) / 20;
-            System.out.println(RESC+" nota(s) de R$ 20,00");
-
-            int RESD = (((N % 100) % 50) % 20) / 10;
-            System.out.println(RESD+" nota(s) de R$ 10,00");
-
-            int RESE = ((((N % 100) % 50) % 20) % 10) / 5;
-            System.out.println(RESE+" nota(s) de R$ 5,00");
-
-            int RESF = (((((N % 100) % 50) % 20) % 10) % 5) / 2 ;
-            System.out.println(RESF+" nota(s) de R$ 2,00");
-
-            int RESG = ((((((N % 100) % 50) % 20) % 10) % 5) % 2) / 1;
-            System.out.println(RESG+" nota(s) de R$ 1,00");
+       //para cada elemento de note dentro da Lista notes executa o comando abaixo
+       for (int note : notes){
+           int count = N / note;
+           System.out.println(count + " nota(s) de R$ " + note + ",00");
+           N %= note;
+       }
     }
 }
