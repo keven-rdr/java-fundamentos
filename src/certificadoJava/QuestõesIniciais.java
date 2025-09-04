b4b756239384d4116bb7760251773989b3c4151b
keven rodrigues <kevenlucas2015@gmail.com>
2025-09-04 17:57:53 -0300

    Questões para a certificação java

---
+++ src/certificadoJava/QuestõesIniciais.java
@@ -0,0 +1,34 @@
+package certificadoJava;
+import java.io.IOException;
+
+public class QuestõesIniciais {
+    public static void main (String[] args) throws IOException {
+        //Questão 1: a saida vai de 0 ate 5, mas ele vai dar erro porque não definiu i como zero
+//        int i = 0;
+//
+//        for(int j = 0; i < 5; i++, j++){
+//            System.out.println(i);
+//        }
+//        //so imprime o 5 porque esta fora do for a impressão
+//        System.out.println(i);
+
+        //Questão 2: tenho 10 espaços que por padrão são gravados como false, seu eu imprimir dessa forma sempre sera false o resultado
+        //caso eu queira alterar algum dos espaços eu preciso chamar o lista e falar esse espaço e true, array[7] = true;
+//        boolean[] array = new boolean[10];
+//        System.out.println(array[7]);
+
+        //Questão 3: Palavras que não fazem parte da linguagem Java, Extends com a letra maiscula, pq no java e case-sensitive, em java deve ser extends
+        // palavra include não faz parte da linguagem java e sim import,
+
+        //Questão 4:
+//        String name = "";
+//
+//        for (int position = 0;  position < 10; position++) {
+//            name = getName(position);
+//        }
+//    }
+//    static String getName(int position){
+//        return new String("Name "+ position);
+//    }
+
+}}
