df766ce8bc9c2ea2c298f183e31e2f8e48e62285
keven rodrigues <kevenlucas2015@gmail.com>
2025-09-12 15:12:16 -0300

    - Adicionando novo exercicio do meu curso do Lenio, exercio de iniciar uma classe que pode ser inicializada varias vezes, ela fica responsável de ficar com toda logica do negocio na camada do service
    - Ajustando o exercicio da LojaProduct para utilizar o construtor e dessa forma obrigar a inicializar o construtor e passar parametros.
    
    - Na mesma classe Product implementar 2 construtores personalizados e 1 padrão

--- src/cursojava/LojaUsaProduct.java
+++ src/cursojava/LojaUsaProduct.java
@@ -1,6 +1,6 @@
 package cursojava;
 
-import entities.Product;
+import entities.  Product;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -9,16 +9,17 @@ import java.io.InputStreamReader;
 public class LojaUsaProduct {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Product product = new Product();
 
         System.out.println("Enter product data:");
-
         System.out.print("Name: ");
-        product.name = String.valueOf(br.readLine());
+        String name = String.valueOf(br.readLine());
         System.out.print("Price: ");
-        product.price = Double.parseDouble(br.readLine());
-        System.out.print("Quantity in stock: ");
-        product.quantity = Integer.parseInt(br.readLine());
+        double price = Double.parseDouble(br.readLine());
+
+
+        //objeto iniciado, agora exigindo os parametros
+        Product product = new Product(name, price);
+
         System.out.println("Product data: "+product);
 
         System.out.print("Enter the number of products to be added in stock: ");
