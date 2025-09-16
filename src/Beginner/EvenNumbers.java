d1d4ed4bd7775bfd4d9f1cb9e37aca8321104e09
keven rodrigues <kevenlucas2015@gmail.com>
2025-09-16 14:15:00 -0300

    - Adicionando mais 3 exercicios utilizando o for para fazer uma verificação e repetição dessa verificação

--- src/Beginner/EvenNumbers.java
+++ src/Beginner/EvenNumbers.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 public class EvenNumbers {
     public static void main (String[] args) throws IOException {
 
-
         for (int i = 1; i < 101 ; i++ ){
             if (i % 2 == 0) {
                 System.out.println(i);
