eebd13344b403f0d661b231e86e78a9133842980
keven rodrigues <kevenlucas2015@gmail.com>
2025-10-08 13:30:16 -0300

    - Refatorando o exercio para seguir o padrão dos demais

--- src/Beginner/SequenceIJ1.java
+++ src/Beginner/SequenceIJ1.java
@@ -4,13 +4,8 @@ import java.io.IOException;
 
 public class SequenceIJ1 {
     public static void main (String[] args) throws IOException {
-        int i = 1;
-        int j = 60;
-
-        while (j >= 0) {
-            System.out.println("I==" + i + " J==" + j);
-            i +=3;
-            j -= 5;
-        }
+       for (int i = 1, j = 60; j >=0; i+=3, j -=5){
+           System.out.println("I=" + i + " J=" + j);
+       }
     }
 }
